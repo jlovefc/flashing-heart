3a4054c712d9c1abdc45a748788b747208a08801
jlovefc <69054898+jlovefc@users.noreply.github.com>
2025-01-02 21:35:50 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
